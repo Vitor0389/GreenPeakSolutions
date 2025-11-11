404dc82a33d558523b4b8a20b9256b616a0cb2d8
Vitor0389 <vitor0389@gmail.com>
2025-11-11 20:01:17 -0300

    Trocando nomes das pastas

--- src/main/java/com/GreenPeak/GreenPeakApplication.java
+++ src/main/java/com/GreenPeak/GreenPeakApplication.java
@@ -1,13 +1,13 @@
-package com.aprendizado;
+package com.GreenPeak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AprendizadoApplication {
+public class GreenPeakApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AprendizadoApplication.class, args);
+		SpringApplication.run(GreenPeakApplication.class, args);
 	}
 
 }
