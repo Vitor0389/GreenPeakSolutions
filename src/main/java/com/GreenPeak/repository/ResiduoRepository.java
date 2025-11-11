1f8a10f3ee7f9536c2c65c7d8de643dc51616dbf
MuriloCSS <murilosao06@gmail.com>
2025-11-11 20:09:24 -0300

    feat: criada classe ResiduoRepository

---
+++ src/main/java/com/GreenPeak/repository/ResiduoRepository.java
@@ -0,0 +1,8 @@
+package com.GreenPeak.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.GreenPeak.model.Residuo;
+
+public interface ResiduoRepository extends JpaRepository<Residuo, Long> {
+}
